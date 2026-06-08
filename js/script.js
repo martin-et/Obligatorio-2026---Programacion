8c218812affa3f0a983da859c516c931ffa836c6
martin-et <arieletchebarne@outlook.com>
2026-06-08 01:18:38 -0300

    Casi pronto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+const frasesRandom = [
+  "Captura el Mundo",
+  "Equipate. Capturá. Creá",
+  "Tu próxima cámara te espera",
+  "Fotografía sin límites",
+];
+
 //Traigo los elementos HTML
+const h1Hero = document.querySelector("#h1-hero");
 const imagenSlider = document.querySelector("#destacados-diapositivas");
 const btnAnterior = document.querySelector("#flechaAnterior");
 const btnSiguiente = document.querySelector("#flechaSiguiente");
@@ -11,6 +19,9 @@ login.addEventListener("click", () => {
   login.disabled = true;
   login.textContent = "Inicion Sesiada".toUpperCase();
 });
+
+let h1Random = Math.floor(Math.random() * frasesRandom.length);
+h1Hero.innerHTML = frasesRandom[h1Random];
 
 let posImagen = 0;
 
@@ -27,7 +38,7 @@ for (let i = 0; i < productos.length; i++) {
     imgDiv.className = "imagen-slider";
     let imagenRandom = Math.floor(Math.random() * productos[i].imagenes.length);
     imgDiv.style.backgroundImage = `url('${productos[i].imagenes[imagenRandom]}')`;
-    
+
     const contenido = document.createElement("div");
     contenido.className = "destacados-contenido";
     contenido.innerHTML = `
@@ -35,7 +46,7 @@ for (let i = 0; i < productos.length; i++) {
     <h3 class="destacados-titulo">${productos[i].nombre}</h3>
     <p class="destacados-precio"><strong>USD</strong> ${productos[i].precio}</p>
     `;
-    
+
     slide.appendChild(imgDiv);
     slide.appendChild(contenido);
     imagenSlider.appendChild(slide);
@@ -47,7 +58,7 @@ slider[0].classList.add("activa");
 
 function siguienteImagen() {
   slider[posImagen].classList.remove("activa");
-  
+
   posImagen++;
   if (posImagen >= slider.length) {
     posImagen = 0;
@@ -57,7 +68,7 @@ function siguienteImagen() {
 
 function anteriorImagen() {
   slider[posImagen].classList.remove("activa");
-  
+
   posImagen--;
   if (posImagen < 0) {
     posImagen = slider.length - 1;
@@ -65,11 +76,31 @@ function anteriorImagen() {
   slider[posImagen].classList.add("activa");
 }
 
+function crearReloj() {
+  let hoy = new Date();
+  let ciberLunes = new Date(2026, 11, 25);
+  let tiempoRestante = ciberLunes - hoy;
 
+  let dias = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
+  let horas = Math.floor((tiempoRestante / (1000 * 60 * 60)) % 24);
+  let minutos = Math.floor((tiempoRestante / (1000 * 60)) % 60);
+  let segundos = Math.floor((tiempoRestante / 1000) % 60);
 
+  if (dias < 10) {
+    dias = "0" + dias;
+  }
+  if (horas < 10) {
+    horas = "0" + horas;
+  }
+  if (minutos < 10) {
+    minutos = "0" + minutos;
+  }
+  if (segundos < 10) {
+    segundos = "0" + segundos;
+  }
 
-
-
-
-
-
+  document.querySelector("#cuentaRegresiva").innerHTML =
+    `<h2>CIBERLUNES: </h2><h3 class="txtDias">${dias} Dias</h3>|<h3 class="txtHoras">${horas} Horas</h3>|<h3 class="txtMInutos">${minutos} Minutos</h3>|<h3 class="txtSegundos">${segundos} Segundos</h3>`;
+}
+crearReloj();
+setInterval(crearReloj, 1000);
